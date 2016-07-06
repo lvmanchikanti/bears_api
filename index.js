f2d22fbc00172844d67b71ff55ff97aed2ae11b6
Lahari <lahu.manchik@ufl.edu>
2016-07-06 18:05:23 -0400

    finished post route

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var lowdb = require('lowdb');
+var uuid = require('uuid');
 var server = express();
 
 var port = process.env.PORT || 8080;
+var db = lowdb('db.json');
+
+db.defaults({bears: []})
+  .value();
 
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({extended: true}));
@@ -15,8 +21,18 @@ server.get('/bears/:id', function(){
   response.send('GET bears :id');
 });
 
-server.post('/bears', function(){
-  response.send('POST bears');
+server.post('/bears', function(request, response){
+  var bear = {
+    id: uuid.v4(),
+    isHungry: false,
+    hasKids: false
+  };
+
+  var result = db.get('bears')
+                .push(bear)
+                .last()
+                .value();
+  response.send(result);
 });
 
 server.put('/bears/:id', function(){
