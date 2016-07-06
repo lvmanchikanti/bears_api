84aa9e5edda9a1bae6104fd581ab019d56030a72
Lahari <lahu.manchik@ufl.edu>
2016-07-05 20:07:02 -0400

    set up api routes

--- index.js
+++ index.js
@@ -7,6 +7,26 @@ var port = process.env.PORT || 8080;
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({extended: true}));
 
+server.get('/bears' , function(){
+  response.send('GET bears');
+});
+
+server.get('/bears/:id', function(){
+  response.send('GET bears :id');
+});
+
+server.post('/bears', function(){
+  response.send('POST bears');
+});
+
+server.put('/bears/:id', function(){
+  response.send('PUT bears :id');
+});
+
+server.delete('/bears/:id', function(){
+  response.send('DELETE bears :id');
+});
+
 
 server.listen(port, function(){
   console.log('Now listening on port: ', port);
