acca07d1b987e87fce036dfb5e348984f927fe27
Lahari <lahu.manchik@ufl.edu>
2016-07-05 20:02:10 -0400

    installed dependencies

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var server = express();
 
 var port = process.env.PORT || 8080;
+
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({extended: true}));
 
 
 server.listen(port, function(){
